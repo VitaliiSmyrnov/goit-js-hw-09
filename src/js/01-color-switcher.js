7d34a424c1fb34bfd8bb34326a9bef1065b5b4e0
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-09 18:55:50 +0200

    'update'

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body');
 
+btnStart.addEventListener('click', onButtonStartClick);
+btnStop.addEventListener('click', onButtonStopClick);
+
+const timerId = setInterval(changeBodyColor, 1000);
+
+function changeBodyColor() {
+   let intervalColor = getRandomHexColor()
+   body.style.backgroundColor = intervalColor;
+}
+
+function onButtonStartClick() {
+   changeBodyColor();
+}
+
+function onButtonStopClick() {
+   clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
