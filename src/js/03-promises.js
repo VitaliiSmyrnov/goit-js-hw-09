5718dac0d5362cd49073601692e216e5cfabd9ac
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-10 13:54:18 +0200

    'update'

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,21 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const formElem = document.querySelector('.form');
 const inputDelayElem = document.querySelector('[name="delay"]');
 const inputDelayStepElem = document.querySelector('[name="step"]');
 const inputAmountElem = document.querySelector('[name="amount"]');
+const btnElem = document.querySelector('.form button');
+
+
+btnElem.addEventListener('click', (e) => {
+  e.preventDefault();
+  const times = inputAmountElem.value;
+  console.log(times);
+  for (let i = 0; i < times; i += 1) {
+    createPromise(position, delay)
+  }
+});
+
 
 
 function createPromise(position, delay) {
