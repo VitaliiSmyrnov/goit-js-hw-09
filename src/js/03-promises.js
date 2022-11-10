12ddfac3f9356b6bebcafdc6777987a3f2ac4428
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-10 20:29:47 +0200

    'update'

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,15 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formElem = document.querySelector('.form');
-const inputDelayElem = document.querySelector('[name="delay"]');
-const inputDelayStepElem = document.querySelector('[name="step"]');
-const inputAmountElem = document.querySelector('[name="amount"]');
-const btnElem = document.querySelector('.form button');
 
-
-btnElem.addEventListener('click', (e) => {
+formElem.addEventListener('submit', (e) => {
   e.preventDefault();
-  const delay = inputDelayElem.value;
-  const step = inputDelayStepElem.value;
-  const amount = inputAmountElem.value;
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
 
-  if ((delay === '' || delay < 0) || (step === '' || step < 0) || (amount === '' || amount < 0)) {
-    Notify.failure("Please, fill in all the fields, value must be positive or zero");
+  if (delay <= 0 || step <= 0 || amount <= 0) {
+    Notify.failure("values must be positive");
   } else {
     for (let position = 1; position <= amount; position += 1) {
       createPromise(position, delay);
@@ -24,10 +19,23 @@ btnElem.addEventListener('click', (e) => {
 });
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise.then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  }).catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
+
+
