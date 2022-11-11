43aa7b2fc496c08272ac4ae24a09dc4ec62e0f51
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-11 13:01:53 +0200

    'update'

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,14 +12,18 @@ formElem.addEventListener('submit', (e) => {
     Notify.failure("values must be positive");
   } else {
     for (let position = 1; position <= amount; position += 1) {
-      createPromise(position, delay);
+      createPromise(position, delay).then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }).catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });;
       delay += step;
     }
   }
 });
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
     setTimeout(() => {
@@ -29,12 +33,6 @@ function createPromise(position, delay) {
         reject({ position, delay });
       }
     }, delay);
-  });
-
-  promise.then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  }).catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
 }
 
